21a6ba64621028d51bf092e1a1740687d03f87c3
Darko Krizic <darko@krizic.net>
2022-11-04 22:20:20 +0100

    First version

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,70 +16,78 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
+	"errors"
+	"github.com/dkrizic/palindrome/logic"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+	"os"
 )
 
+var lowercase = false
+var silent = false
 var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "palindrome",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Checks if a given string is a palindrome",
+	Long: `Checks if a given string is a palindrome. 
+For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+palindrome racecar
+palindrome kayak
+palindrome aibohphobia
+palindrome radar
+palindrome madam
+palindrome level
+palindrome rotor
+palindrome civic
+palindrome kayak
+palindrome reviver
+palindrome racecar
+palindrome redder
+palindrome madam
+palindrome civic
+`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		if len(args) == 0 {
+			cmd.Help()
+			return nil
+		}
+		palindrome := args[0]
+		isPalindrome, inputString := logic.IsPalindrome(palindrome, lowercase)
+		if isPalindrome {
+			if !silent {
+				cmd.Printf("Yes, %s is a palindrome\n", inputString)
+			}
+			return nil
+		} else {
+			if !silent {
+				cmd.Println("No, %s is not a palindrome\n", inputString)
+			}
+			return errors.New("No palindrome")
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
 }
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.palindrome.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&lowercase, "lowercase", "l", false, "Converts the string to lowercase")
+	rootCmd.PersistentFlags().BoolVarP(&silent, "silent", "s", false, "Silent mode")
+	viper.BindEnv("lowercase", "PALINDROME_LOWERCASE")
+	viper.BindEnv("silent", "PALINDROME_SILENT")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".palindrome" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".palindrome")
-	}
-
 	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
